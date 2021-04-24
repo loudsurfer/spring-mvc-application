ae2560eabdbfc3117d32562c6e35c7e944ff50e8
loudsurfer <loudsurfer@gmail.com>
2021-04-24 12:03:05 +0300

    перевязал конфигурацию на java classes

---
+++ src/main/java/com/techno/loud/config/MySpringMvcDispatcherServletInItializer.java
@@ -0,0 +1,21 @@
+package com.techno.loud.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInItializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
